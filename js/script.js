feb8ee20090a45808c9033bf52bfaf99dfb69225
manu-devops98 <manuelcappello7@gmail.com>
2021-12-17 13:59:24 +0100

    add click su input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
+        note: '',
         lists: [
             {
                 title: 'Spesa',
@@ -27,6 +28,22 @@ const app = new Vue({
     methods: {
         deleteBox: function(index) {
             this.lists.splice(index,1);
+        },
+        newBox: function() {
+            this.check = 'fas fa-times red';
+            if (this.note != '') {
+                this.title = {
+                    title: this.firstLetterUpper(this.note),
+                    check: this.check
+                };
+                this.lists.unshift(this.title);
+                this.note = '';
+            }
+        },
+        firstLetterUpper: function(word) {
+            let string = word;
+            let stringTwo = string.charAt(0).toUpperCase() + string.slice(1);
+            return stringTwo;
         }
     }
   });
