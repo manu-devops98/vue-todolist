379365dd5b2e6c19d065779efc28029883ee5dac
manu-devops98 <manuelcappello7@gmail.com>
2021-12-17 12:45:51 +0100

    add box dinamicamente

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 const app = new Vue({
     el: '#app',
     data: {
-      
+        lists: [
+            {
+                title: 'Spesa',
+                check: 'fas fa-times red'
+            },
+            {
+                title: 'Progetto',
+                check: 'fas fa-check green'
+            },
+            {
+                title: 'Regali',
+                check: 'fas fa-check green'
+            },
+            {
+                title: 'Chiamare Agenzia',
+                check: 'fas fa-times red'
+            },
+            {
+                title: 'Lavare Auto',
+                check: 'fas fa-check green'
+            }
+        ]
     }
   })
